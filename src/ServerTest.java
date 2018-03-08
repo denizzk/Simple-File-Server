7f4f510c3319712426da753488a06b7c8c1207a4
kaymakf <kaymakaf@gmail.com>
2018-03-08 15:36:55 +0300

    3. hafta

--- src/ServerTest.java
+++ src/ServerTest.java
@@ -1,39 +1,63 @@
+import java.awt.image.BufferedImage;
+import java.io.File;
 import java.io.IOException;
+import java.util.Enumeration;
 
+import javax.imageio.ImageIO;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.eclipse.jetty.http.HttpStatus;
 import org.eclipse.jetty.server.Request;
 import org.eclipse.jetty.server.Server;
 import org.eclipse.jetty.server.handler.AbstractHandler;
+import org.eclipse.jetty.server.handler.ContextHandler;
 
 public class ServerTest extends AbstractHandler
 {
+
     @Override
     public void handle( String target,
                         Request baseRequest,
                         HttpServletRequest request,
-                        HttpServletResponse response ) throws IOException,
-                                                      ServletException
+                        HttpServletResponse response ) throws IOException, ServletException
     {
-        // Declare response encoding and types
-        response.setContentType("text/html; charset=utf-8");
 
-        // Declare response status code
-        response.setStatus(HttpServletResponse.SC_OK);
+        //response.setCharacterEncoding("iso-2022-jp");
 
-        // Write back response
-        response.getWriter().println("<h1>Hello World</h1>");
+        response.getWriter().write("<h1>Hello World</h1>");
+        Enumeration<String> parameterNames = request.getParameterNames();
+        while (parameterNames.hasMoreElements()){
+            String name = (String) parameterNames.nextElement();
+            String value = request.getParameter(name).toString();
+            response.getWriter().write(String.format("%s:%s\n", name, value));
+        }
 
-        // Inform jetty that this request has now been handled
+        response.getWriter().write("<br>");
+        response.getWriter().write(String.format("Method:%s<br>", request.getMethod()));
+        response.getWriter().write(String.format("Request URI:%s<br>", request.getRequestURI()));
+        response.getWriter().write(String.format("Context:%s<br>", request.getContextPath()));
+        response.getWriter().write(String.format("Path:%s<br>", request.getPathInfo()));
+        response.getWriter().write(String.format("Local Host:%s<br>", request.getLocalName()));
+        response.getWriter().write(String.format("Remote Host:%s<br>", request.getRemoteHost()));
+
+        //response.sendRedirect("http://google.com");
+
+        //response.setStatus(HttpStatus.FORBIDDEN_403);
+
         baseRequest.setHandled(true);
+
     }
 
     public static void main( String[] args ) throws Exception
     {
         Server server = new Server(8080);
-        server.setHandler(new ServerTest());
+
+        ContextHandler context = new ContextHandler();
+        context.setContextPath("/merhabaaa");
+        context.setHandler(new ServerTest());
+        server.setHandler(context);
 
         server.start();
         server.join();
