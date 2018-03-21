78698d62e82f0f886bdeac136a09c380fc11a6fe
denizkarakaya <denizkarakaya@rubygamestudio.com>
2018-03-21 21:20:50 +0300

    4. week

--- src/ServerTest.java
+++ src/ServerTest.java
@@ -1,5 +1,7 @@
 import java.awt.*;
+import java.awt.color.ColorSpace;
 import java.awt.image.BufferedImage;
+import java.awt.image.ColorConvertOp;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.OutputStream;
@@ -14,76 +16,81 @@ import org.eclipse.jetty.server.Request;
 import org.eclipse.jetty.server.Server;
 import org.eclipse.jetty.server.handler.AbstractHandler;
 
-public class ServerTest extends AbstractHandler
-{
-    public void handle(String string, Request rqst, HttpServletRequest request, HttpServletResponse response){
+public class ServerTest extends AbstractHandler {
+	public void handle(String string, Request rqst, HttpServletRequest request, HttpServletResponse response) {
 
+		int width = Integer.parseInt(request.getParameter("width"));
+		int height = Integer.parseInt(request.getParameter("height"));
+		String color = request.getParameter("color");
 
-        int width = Integer.parseInt(request.getParameter("width"));
-        int height = Integer.parseInt(request.getParameter("height"));
-        String color = request.getParameter("color");
+		BufferedImage bufferedImage = null;
 
-        BufferedImage bufferedImage = null;
+		 response.setHeader("Content-Type", "image/jpg");
 
-        response.setHeader("Content-Type", "image/jpg");
+		String fileName = "./img/bears.jpg";
 
-        String fileName = "./" + request.getPathInfo();
+		try {
+			String scaledImagePath = scale(fileName, width, height, "newBears.jpg");
 
-        try {
-            String[] nfn = fileName.split("/");
-            String scaledImagePath = scale(fileName, width, height, "scaled" + nfn[nfn.length - 1]);
+			if (color.equals("grey"))
+				scaledImagePath = greyFilter(ImageIO.read(new File(scaledImagePath)), "greyBears.jpg");
 
-            FileInputStream fis = new FileInputStream(scaledImagePath);
+			FileInputStream fis = new FileInputStream(scaledImagePath);
 
-            sendFile(fis, response.getOutputStream());
-            bufferedImage = ImageIO.read(new File(scaledImagePath));
-            ImageIO.write(bufferedImage, "jpg", response.getOutputStream());
+			sendFile(fis, response.getOutputStream());
+			bufferedImage = ImageIO.read(new File(scaledImagePath));
+			ImageIO.write(bufferedImage, "jpg", response.getOutputStream());
 
-        } catch (Exception e) {
-            Logger.getLogger(ServerTest.class.getName()).log(Level.SEVERE, null, e);
-        }
+		} catch (Exception e) {
+			Logger.getLogger(ServerTest.class.getName()).log(Level.SEVERE, null, e);
+		}
 
-    }
+	}
 
+	public static void main(String[] args) throws Exception {
+		Server server = new Server(8080);
+		server.setHandler(new ServerTest());
+		server.start();
+		server.join();
+	}
 
-    public static void main( String[] args ) throws Exception
-    {
-        Server server = new Server(8080);
-        server.setHandler(new ServerTest());
-        server.start();
-        server.join();
-    }
+	public String scale(String path, int width, int height, String filename) throws Exception {
+		BufferedImage org = ImageIO.read(new File(path));
+		BufferedImage resizedCopy = createResizedCopy(org, width, height, true);
+		File tosave = new File("img\\" + filename);
+		ImageIO.write(resizedCopy, "jpg", tosave);
+		return tosave.getAbsolutePath();
 
+	}
 
-    public String scale(String path, int width, int height, String filename) throws Exception{
-        BufferedImage org = ImageIO.read(new File(path));
-        BufferedImage resizedCopy = createResizedCopy(org, width, height, true);
-        File tosave = new File("img/" + filename);
-        ImageIO.write(resizedCopy, "jpg", tosave);
-        return tosave.getAbsolutePath();
+	private BufferedImage createResizedCopy(Image org, int scaledWidth, int scaledHeight, boolean preserveAlpha) {
+		System.out.println("resizing...");
+		int imageType = preserveAlpha ? BufferedImage.TYPE_INT_RGB : BufferedImage.TYPE_INT_ARGB;
+		BufferedImage scaledBI = new BufferedImage(scaledWidth, scaledHeight, imageType);
+		Graphics2D g = scaledBI.createGraphics();
+		if (preserveAlpha)
+			g.setComposite(AlphaComposite.Src);
+		g.drawImage(org, 0, 0, scaledWidth, scaledHeight, null);
+		g.dispose();
+		return scaledBI;
+	}
 
-    }
+	public static void sendFile(FileInputStream fin, OutputStream out) throws Exception {
+		byte[] buffer = new byte[1024];
+		int bytesRead;
 
-    private BufferedImage createResizedCopy(Image org, int scaledWidth, int scaledHeight, boolean preserveAlpha){
-        System.out.println("resizing...");
-        int imageType = preserveAlpha ? BufferedImage.TYPE_INT_RGB : BufferedImage.TYPE_INT_ARGB;
-        BufferedImage scaledBI = new BufferedImage(scaledWidth, scaledHeight, imageType);
-        Graphics2D g = scaledBI.createGraphics();
-        if (preserveAlpha)
-            g.setComposite(AlphaComposite.Src);
-        g.drawImage(org, 0, 0, scaledWidth, scaledHeight, null);
-        g.dispose();
-        return scaledBI;
-    }
+		while ((bytesRead = fin.read(buffer)) != -1)
+			out.write(buffer, 0, bytesRead);
+		fin.close();
+	}
 
-    public static void sendFile(FileInputStream fin, OutputStream out)throws Exception{
-        byte[] buffer = new byte[1024];
-        int bytesRead;
+	public static String greyFilter(BufferedImage coloredImage, String filename) throws Exception {
+		ColorSpace cs = ColorSpace.getInstance(ColorSpace.CS_GRAY);
+		ColorConvertOp op = new ColorConvertOp(cs, null);
+		BufferedImage image = op.filter(coloredImage, null);
 
-        while ((bytesRead = fin.read(buffer)) != -1)
-            out.write(buffer, 0, bytesRead);
-        fin.close();
-    }
-
-
+		File tosave = new File("img\\" + filename);
+		ImageIO.write(image, "jpg", tosave);
+		return tosave.getAbsolutePath();
+	}
 }
